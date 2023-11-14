812427f77431d3017ba140900ce007e66da59e1d
Juan Valentin <valentin.juan05@gmail.com>
2023-11-14 12:32:22 -0300

    add interator loops exercise

---
+++ src/main/kotlin/controlFlow/animalLoopIterators.kt
@@ -0,0 +1,19 @@
+package controlFlow
+
+class Animal(val name: String)
+
+class Zoo(val animals: List<Animal>) {
+    operator fun iterator(): Iterator<Animal> {
+        return animals.iterator()
+    }
+}
+
+fun main() {
+
+    val animals = listOf(Animal("zebra"), Animal("lion"))
+    val zoo = Zoo(animals)
+
+    for (animal in zoo) {
+        println("Watch out, it's a ${animal.name}")
+    }
+}
