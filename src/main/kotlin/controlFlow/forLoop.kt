52b58b1c88af3f3b014db3384898c42595dfbc01
Juan Valentin <valentin.juan05@gmail.com>
2023-11-13 23:36:28 -0300

    add forLoop exercise

---
+++ src/main/kotlin/controlFlow/forLoop.kt
@@ -0,0 +1,11 @@
+package controlFlow
+
+fun main(){
+    val cakes = listOf("carrot", "cheese", "chocolate")
+    
+    for (cake in cakes) {
+        println("Yummy, it`s a $cake cake!")
+    }
+}
+
+class forLoop
